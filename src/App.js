22e7f59356c2132dd023ce90e8c9eedb0d2e431f
ivnNear <lolwto.gg@gmail.com>
2023-04-09 16:54:29 +0200

    add loading component

--- src/App.js
+++ src/App.js
@@ -2,33 +2,51 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import axios from 'axios';
 import { AllCards } from './components/AllCards';
+import { Loading } from './components/Loading';
 
 const url = 'https://course-api.com/react-tours-project';
 function App() {
   const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get(url).then((res) => setTours(res.data));
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const items = await response.json();
+      setTours(items);
+      setLoading(false);
+    };
+    fetchData();
   }, []);
-  
-  
-  const filterTour = (tit) => {
-    setTours(tours.filter((el) => el.name !== tit))
-  
-}
-  
-if(tours.length === 0){
-  return <div className="wrapper">
-    <h1>no tours left</h1>
-    <button onClick={() => axios.get(url).then((res) => setTours(res.data)) }>reset tours</button>
-  </div>
-}
 
+  
+
+  const filterTour = (tit) => {
+    setTours(tours.filter((el) => el.name !== tit));
+  };
+  if(loading){
+    return <Loading />
+  }
+
+  if (tours.length === 0) {
+    return (
+      <div className='wrapper'>
+        <h1>no tours left</h1>
+        <button
+          onClick={() => axios.get(url).then((res) => setTours(res.data))}
+        >
+          reset tours
+        </button>
+      </div>
+    );
+  }
 
   return (
     <div className='wrapper'>
-      <AllCards tours={tours} filterTour={filterTour} />
-    
+      <AllCards
+        tours={tours}
+        filterTour={filterTour}
+      />
     </div>
   );
 }
